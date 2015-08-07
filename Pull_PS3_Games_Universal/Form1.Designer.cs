b6b9ded95c0270738daa2ddd89088e26ccd7ea6c
Mr3us <kmlourenco91@gmail.com>
2015-08-07 17:39:28 -0400

    The entire project
    This is all the code with the chromedriver and other library necessary

---
+++ Pull_PS3_Games_Universal/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Pull_PS3_Games_Universal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uname = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.helplabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.folderPath = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(12, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(12, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // uname
+            // 
+            this.uname.Location = new System.Drawing.Point(96, 79);
+            this.uname.Name = "uname";
+            this.uname.Size = new System.Drawing.Size(100, 20);
+            this.uname.TabIndex = 2;
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(96, 120);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(100, 20);
+            this.pass.TabIndex = 3;
+            this.pass.UseSystemPasswordChar = true;
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(96, 181);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 4;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // helplabel
+            // 
+            this.helplabel.AutoSize = true;
+            this.helplabel.Location = new System.Drawing.Point(30, 223);
+            this.helplabel.Name = "helplabel";
+            this.helplabel.Size = new System.Drawing.Size(0, 13);
+            this.helplabel.TabIndex = 5;
+            this.helplabel.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Location = new System.Drawing.Point(41, 9);
+            this.label4.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 39);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Type in you\'re username and password, and a folder where I can print the list of " +
+    "games.";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(96, 151);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(100, 20);
+            this.filePath.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Enabled = false;
+            this.label5.Location = new System.Drawing.Point(12, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "File Location:";
+            // 
+            // folderPath
+            // 
+            this.folderPath.Location = new System.Drawing.Point(191, 149);
+            this.folderPath.Name = "folderPath";
+            this.folderPath.Size = new System.Drawing.Size(64, 23);
+            this.folderPath.TabIndex = 9;
+            this.folderPath.Text = "Path";
+            this.folderPath.UseVisualStyleBackColor = true;
+            this.folderPath.Click += new System.EventHandler(this.folderPath_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.folderPath);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.helplabel);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.uname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Every PS Purchase Ever";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox uname;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label helplabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button folderPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Pull_PS3_Games_Universal/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Pull_PS3_Games_Universal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.uname.Text.CompareTo("") == 0 && this.pass.Text.CompareTo("") == 0 && this.filePath.Text.CompareTo("") == 0)
+            {
+                this.helplabel.Text = "Please fill in all fields";
+            }
+            else
+            {
+                int ret = pullGames(this.uname.Text, this.pass.Text, this.filePath.Text);
+                if (ret == 0)
+                    this.helplabel.Text = "Success! Check the folder for a file called 'PSPurchaseList'";
+                if (ret == 1)
+                    this.helplabel.Text = "Failure! Please check your username and password, then retry.";
+                if (ret == 2)
+                    this.helplabel.Text = "Failure!";
+                this.helplabel.Visible = true;
+
+            }
+
+        }
+
+        private int pullGames(string uname, string pword, string fpath)
+        {
+            string exePath = Application.ExecutablePath;
+            string[] path = exePath.Split('\\');
+            string cdPath = "";
+            for (int i = 0; i < path.Length - 1; i++)
+                cdPath += path[i] + "\\";
+            ChromeDriver cd;
+            try
+            {
+                cd = new ChromeDriver(cdPath);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Put chromedriver in: " + cdPath);
+                return 2;
+            }
+
+            cd = login(cd, uname, pword);
+
+            //Now go to transaction summary page
+            System.Threading.Thread.Sleep(2000); // Give the page time to load
+            IWebElement e;
+            e = cd.FindElementById("transactionHistoryLink");
+            e.Click();
+            //Now change the date to 2010, since psplus has only been around since then
+            System.Threading.Thread.Sleep(2000); // Give the page time to load
+
+            e = cd.FindElementById("largeFilterStartInput");
+            e.Clear();
+            e.SendKeys("01/01/2010");
+            //Now update table
+            e = cd.FindElementById("largeFilterUpdateDateFilterButton");
+            e.Click();
+            System.Threading.Thread.Sleep(2000);
+
+
+            List<string> AllYourGames = new List<string>();
+            try
+            {
+                AllYourGames = pullGamesHelper(cd);
+                File.WriteAllLines(fpath + "\\PSPurchaseList.txt", AllYourGames);
+                return 0;
+            }
+            catch (Exception)
+            {
+                return 2;
+            }
+        }
+
+
+        private ChromeDriver login(ChromeDriver cd, string uname, string pword)
+        {
+            cd.Url = "https://account.sonyentertainmentnetwork.com/login.action";
+            cd.Navigate();
+            IWebElement e = cd.FindElementById("signInInput_SignInID");
+            e.SendKeys(uname);
+            e = cd.FindElementById("signInInput_Password");
+            e.SendKeys(pword);
+            e = cd.FindElementById("signInButton");
+            e.Click();
+            return cd;
+        }
+
+        private List<string> pullGamesHelper(ChromeDriver cd)
+        {
+            List<string> AllYourGames = new List<string>();
+            IWebElement e;
+            string purchaseTitle = "";
+            bool anotherRow = true;
+            int index = 0;
+            while (anotherRow)
+            {
+                index++;
+                try
+                {
+                    e = cd.FindElementById("transactionDetailsRow-" + index);
+                    e.Click();
+                    System.Threading.Thread.Sleep(1000);
+
+                    try
+                    {
+                        ReadOnlyCollection<IWebElement> roc = cd.FindElements(By.Id("itemTitle-0"));
+                        foreach (IWebElement iwe in roc)
+                        {
+                            purchaseTitle = iwe.Text;
+                            AllYourGames.Add(purchaseTitle);
+                        }
+                        cd.Navigate().Back();
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                    catch (Exception)
+                    {
+                        purchaseTitle = "This wasn't a game/movie";
+                        cd.Navigate().Back();
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                    //AllYourGames.Add(purchaseTitle);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("The program has finished gathering the data...");
+                    anotherRow = false;
+                }
+
+            }
+            cd.Close();
+            cd.Dispose();
+            return AllYourGames;
+        }
+
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderPath_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.filePath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
